e81aa60f5b1019caaa95a57aaae632da38783e0f
comeauc <cameron.comeau@gmail.com>
2014-09-16 12:04:33 -0600

    to push onto github

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 CSCI_3308
 =========
-
+Cameron Comeau
+CSCI 3308
 Fall 2014
+
+Lab 3 assignment: understanding git and github
---
+++ lab1_code.cpp
@@ -0,0 +1,48 @@
+#include <iostream.h>
+#include <math.h>
+
+
+//Cameron Comeau
+//Luis Olivas
+//11 AM Recitation
+
+void letters(num int);
+void warp(int n){
+	int  n[] = (int) "PUZZLEITWHATSUPIELETONTHEMMAPTSEAGLOMOF7S"; 
+
+while(num > 300){
+	if(args.length > 1) {
+		num -= 197;	
+	
+		while(num > 1) {
+			int place = warp(num);
+			num = (int) args[0];
+			int letter = n[-place];	
+		}
+	} else {
+		yield letter;
+}		if(num % 7 == 0){
+			break
+
+return		}
+		num = 4096;
+		num = num // 2;
+
+	}
+
+
+	while(num > 60){
+		cout << letter << endl;
+		num = num / 7
+	}
+	
+	cout << "" << endl;
+	return ceil(2 * num / 3);
+}}	
+
+};
+int main(){
+
+}
+
+
